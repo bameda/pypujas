2018260841070543b01b7a928cc0c4d6ec4d297e
Román Rodríguez <romanrdgz@gmail.com>
2017-02-28 23:17:12 +0100

    Add files via upload

--- pyPujas/spiders/pujasboe.py
+++ pyPujas/spiders/pujasboe.py
@@ -32,22 +32,33 @@ class PujasboeSpider(scrapy.Spider):
     ]
    
     def parse(self, response):
-        # Parse results from first page
-        self.parse_results(response)
+        # Get iterable URL of current search results
+        url = response.xpath('//ul[@class="navlist"]/li/a[@class="current"]/@href').extract()[0]
+        # Detect position of first '-' char in URL
+        pos = url.index('-')
+        # Remove that data from URL
+        url = url[:pos] + '-{}-{}'
         
-        # Once we are done with listed pujas, we search next page link
-        url = response.xpath('//span[@class="pagSig"]/preceding::a[1]/@href')
-        full_url = urllib.parse.urljoin('https://' + self.allowed_domains[0], url.extract()[0])
-        yield scrapy.Request(full_url, self.parse_results)
+        # Get the number of search results
+        results_text = response.xpath('//div[@class="paginar"]/ul/li/text()').extract()[0]
+        n_results = int(results_text.split()[-1])
+        results_per_page = 40
+        start_result = 0
+        
+        # Start visiting each page of 40 results and parsing its contents
+        while start_result < n_results:
+            next_url = urllib.parse.urljoin('https://' + self.allowed_domains[0], url.format(start_result, results_per_page))
+            start_result += results_per_page
+            yield scrapy.Request(next_url, self.parse_results)
         
     def parse_results(self, response):
         items = []
         # Get the link for each puja, and use parse_puja as a callback for those requests
-        links_pujas = response.xpath('//div[@class="enlacesMas"]/a[@class="resultado-busqueda-link-defecto"]/@href')
+        links_pujas = response.xpath('//div[@class="enlacesMas"]/a[@class="resultado-busqueda-link-defecto"]/@href').extract()
         for url in links_pujas:
             item = PypujasItem()
-            full_url = urllib.parse.urljoin('https://' + self.allowed_domains[0], url.extract())
-            request = scrapy.Request(url=full_url, callback=self.parse_table_puja)
+            full_url = urllib.parse.urljoin('https://' + self.allowed_domains[0], url)
+            request = scrapy.Request(url=full_url, callback=self.parse_table_puja, meta={'item': item})
             request.meta['item'] = item
             yield request
             
@@ -63,21 +74,30 @@ class PujasboeSpider(scrapy.Spider):
             
     def parse_table_puja(self, response):
         item = response.meta['item']
-        table_rows = response.xpath('//table[@class="datosSubastas"]/tbody/tr')
-        print('TABLE ROWS: ', table_rows) # TODO empty
+        table_rows = response.xpath('//table[@class="datosSubastas"]/tr')
+
         for row in table_rows:
-            row_id = row.xpath('th/text()').extract()
-            row_value = row.xpath('td/strong/text()').extract()
-            
-            print('ROW: ', row_id, row_value)
-            
-            if row_id in row_ids_dict.keys():
-                item[row_ids_dict[row_id]] = row_value
+            try:
+                row_id = row.xpath('th/text()').extract()[0]
+                row_value = row.xpath('td/strong/text()|td/text()').extract()[0].strip().rstrip()
+                if not row_value:
+                    print('coco-1')
+                    row_value = row.xpath('td/strong/text()').extract()[0].strip().rstrip()
+                if not row_value:
+                    print('coco-2')
+                    row_value = row.xpath('td/a/@href').extract()[0]
+                    
+                print('ROW: ', row_id, row_value)
+                
+                if row_id in row_ids_dict.keys():
+                    item[row_ids_dict[row_id]] = row_value
+            except Exception as err:
+                print('ERROR: ' + str(err))
         
         return item
 
     def parse_amounts_puja(self, response):
         item = response.meta['item']
-        highest_bid = response.xpath('//div[@class="bloqueSubasta"]/h3[*="Puja más alta en esta subasta"]/following::p/span[@class="destacaNegrita"]/text()').extract()
+        highest_bid = response.xpath('//div[@class="bloqueSubasta"]/h3[*="Puja más alta en esta subasta"]/following::p/span[@class="destacaNegrita"]/text()').extract()[0]
         item['highest_bid'] = 0 if(highest_bid == 'Subasta sin pujas') else highest_bid
         return item
